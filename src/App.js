5bf4a898355eff109e791da8a1032213c5af3ae3
Aphenphos <MaxwillWinters@gmail.com>
2022-09-26 15:12:19 -0400

    books route works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/books">
+            <BookList />
+          </Route>
+          <Route path="/books/:id">
+            <BookDetail />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
